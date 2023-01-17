a5e7c3a3ea917afe5129f35b39ba439a13f875a3
Fiseha Abebe <famarkfish79@gmail.com>
2023-01-17 21:08:18 +0300

    added sample image without formatting

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -3,6 +3,8 @@ import { useRef, useEffect, useState, React } from "react";
 
 import { Link } from "react-router-dom";
 
+const hero_img = require("../assets/hero_house.jpg");
+
 
 
 
@@ -20,10 +22,14 @@ const Home = () => {
   return (
     <div className="Home" id="Home">
       
+      <div className="hero">
+      <div className="hero-img">
+          <img src={hero_img} alt="hero-img"/>
+        </div>
+      </div>
       
       
       
-      Start
     
 
       
