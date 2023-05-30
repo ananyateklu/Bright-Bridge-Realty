d638b2cf5b4d647849d4d7b8fb92e098669ff194
Ananya Teklu <teklu009@morris.umn.edu>
2023-05-30 13:14:26 -0500

    Stop rendering for mobile and show warning

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,21 @@ import Contact from './components/Contact/Contact'
 import Footer from './components/Footer';
 import Mortgage from './components/Mortgage/Mortgage';
 import Sold from './components/Sold/Sold';
-
+import house from "../src/assets/mobile.jpg";
+import logo from '../src/assets/BBLogo.png';
 
 function App() {
+  const isMobile = window.matchMedia("only screen and (max-width: 1200px)").matches;
+
+  if (isMobile) {
+    return <div className="Home">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className="mobilePrompt">Sorry, this website is only available on desktop devices.</div>
+      <div className="mobile">
+        <img src={house} alt="house" />
+      </div>
+    </div>
+  }
   return (
     <div className="App">
       <BrowserRouter>
@@ -21,7 +33,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<HouseSearchResult />} />
           <Route path="/house-details/:zpid" element={<HouseDetails />} />
-          <Route path="/about" element={<About  />} />
+          <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/mortgage" element={<Mortgage />} />
           <Route path='/sold' element={<Sold />} />
